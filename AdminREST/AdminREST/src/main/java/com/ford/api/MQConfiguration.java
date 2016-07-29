737f50ef51437353b0e9d02a2bf4c527a16b3b7d
SARUNANA <arunspec5@gmail.com>
2016-07-29 11:33:27 +0530

    Commit all

--- AdminREST/AdminREST/src/main/java/com/ford/api/MQConfiguration.java
+++ AdminREST/AdminREST/src/main/java/com/ford/api/MQConfiguration.java
@@ -2,12 +2,14 @@ package com.ford.api;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.JsonMessageConverter;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.service.common.AmqpServiceInfo;
@@ -15,7 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import com.ford.api.Receiver;
+import com.pivotal.spring.REST.User;
 
 @Configuration
 public class MQConfiguration {
@@ -48,12 +50,18 @@ final static String queueName = "spring-boot";
 		return container;
 	}
 	   @Bean
-	    Receiver receiver() {
-	        return new Receiver();
+	    MessageListener receiver() {
+	       
+				MessageListener ml= (message) -> {
+					JsonMessageConverter jmc = new JsonMessageConverter();
+			    	User user = (User)jmc.fromMessage(message);
+					System.out.println("Received <" + user.getUserId() + "    "+ user.getUserName() + ">");
+			};
+			return ml;
 	    }
 
 		@Bean
-		MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		MessageListenerAdapter listenerAdapter(MessageListener receiver) {
 			return new MessageListenerAdapter(receiver, "receiveMessage");
 		}
 		@Bean
